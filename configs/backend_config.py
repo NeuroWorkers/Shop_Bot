1473b4c6c954b81869f9c35c123decab1a8d0fbc
Sv163Sam <Tankruuus@gmail.com>
2025-05-01 19:57:29 +0400

    ref conf

--- configs/backend_config.py
+++ configs/backend_config.py
@@ -10,14 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(os.path.join(BASE_DIR, "install", ".env"))
 
-"""
 http_proxy = os.environ.get("HTTP_PROXY") or "http://172.29.177.25:3128"
 https_proxy = os.environ.get("HTTPS_PROXY") or "https://172.29.177.25:3128"
 proxies = {
     "http": http_proxy,
     "https": https_proxy,
 }
-"""
 
 DATA_PATH = os.path.join(BASE_DIR, "database", "shop_products.json")
 VECTOR_STORE_PATH = os.path.join(BASE_DIR, "backend", "vector_store")
